645a8ce95415cfd14ea629f41164708144bf508d
admls <adam.the.sherwood@gmail.com>
2019-06-08 13:41:48 +0100

    effect routing to about and demo

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 export default () => (
   <ul>
     <li>
-      <Link href='/b' as='/a'>
-        <a>a</a>
+      <Link href="/about" as="/about">
+        <a>about</a>
       </Link>
     </li>
     <li>
-      <Link href='/a' as='/b'>
-        <a>b</a>
+      {/* This doesn't work for some reason */}
+      <Link href="/demo" as="/demo">
+        {/* <a href="/demo">demo</a> */}
+        <a>demo</a>
       </Link>
     </li>
-    <li>
+    {/* <li>
       <Link href={{ pathname: '/posts', query: { id: '2' } }} as='/posts/2'>
         <a>post #2</a>
       </Link>
-    </li>
+    </li> */}
   </ul>
-)
+);
--- server.js
+++ server.js
@@ -9,17 +9,17 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  server.get("/a", (req, res) => {
-    return app.render(req, res, "/a", req.query);
+  server.get("/about", (req, res) => {
+    return app.render(req, res, "/about", req.query);
   });
 
-  server.get("/b", (req, res) => {
-    return app.render(req, res, "/b", req.query);
+  server.get("/demo", (req, res) => {
+    return app.render(req, res, "/demo", req.query);
   });
 
-  server.get("/posts/:id", (req, res) => {
-    return app.render(req, res, "/posts", { id: req.params.id });
-  });
+  // server.get("/posts/:id", (req, res) => {
+  //   return app.render(req, res, "/posts", { id: req.params.id });
+  // });
 
   server.get("*", (req, res) => {
     return handle(req, res);
