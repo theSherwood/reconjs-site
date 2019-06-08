dac08abdfeaa0d4d0f9db643dbf1649472f79343
admls <adam.the.sherwood@gmail.com>
2019-06-08 21:45:13 +0100

    improves error readout

--- components/EditorError/EditorError.js
+++ components/EditorError/EditorError.js
@@ -4,16 +4,22 @@ const EditorError = props => {
   let errs;
   if (Array.isArray(errors)) {
     errs = errors.map(error => {
-      let errStr = "illicit: " + error.illicit;
+      let errStr =
+        "ReconError: '" +
+        error.illicit +
+        "' is an illicit word (line: " +
+        error.line +
+        ", column: " +
+        error.startColumn +
+        ")";
       return errStr;
     });
-  } else {
-    errs = errors;
+  } else if (errors.hasOwnProperty("message")) {
+    errs = [errors.name + ": " + errors.message];
   }
+
   return (
-    <div>
-      <p>{errs}</p>
-    </div>
+    <div>{errs.length ? errs.map(err => <p key={err}>{err}</p>) : null}</div>
   );
 };
 
