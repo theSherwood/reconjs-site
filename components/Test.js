b7431c3dd84ca62e43586cf7465ee2860a21568e
admls <adam.the.sherwood@gmail.com>
2019-06-10 21:58:32 +0100

    test the api and database connection

--- components/Test.js
+++ components/Test.js
@@ -1,1 +1,21 @@
-export default () => <p>Test</p>;
+import React, { useEffect } from "react";
+
+export default () => {
+  useEffect(() => {
+    fetch("/api/breach.js", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        code: "this is a test value posted to the database"
+      })
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return <p>Testing... Check the console...</p>;
+};
