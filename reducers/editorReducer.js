d6bff79472f797ea1a4fdc269d89dbafcba02f99
admls <adam.the.sherwood@gmail.com>
2019-06-13 19:22:19 +0100

    remove long intro text from editor screen

--- reducers/editorReducer.js
+++ reducers/editorReducer.js
@@ -1,6 +1,5 @@
 export const initialEditor = {
-  editorValue:
-    "/*\nAttempt to get past ReconJS's static security checks:\n\nChange the value of window.target from within the arcade console and add your name to the list of victors!\n*/",
+  editorValue: "// TODO: change the value of window.target\n",
   errors: {}
 };
 
