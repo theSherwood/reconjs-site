f83f8ab07d9b20021388443965d7e82c2d213e21
admls <adam.the.sherwood@gmail.com>
2019-06-13 13:35:10 +0100

    adds markup for adding name

--- components/Victors/Victors.js
+++ components/Victors/Victors.js
@@ -5,35 +5,82 @@ function randColor(colors) {
 }
 
 function rainbowize(string) {
-  return string
-    .split("")
-    .map(char => <span style={{ color: randColor(colors) }}>{char}</span>);
+  return string.split("").map((char, i) => (
+    <span key={i} style={{ color: randColor(colors) }}>
+      {char}
+    </span>
+  ));
 }
 
 const Victors = ({ victors, newVictor }) => {
   return (
-    <div>
+    <div className="victor-container">
+      {newVictor ? (
+        <div>
+          <h3 className="text-center">{rainbowize(" You  beat  Recon JS ")}</h3>
+          <p className="text-center">
+            {
+              "Enter your name and thank you for helping us make ReconJS better!"
+            }
+          </p>
+          <input className="input-name" size="10" />
+        </div>
+      ) : null}
       {victors ? (
-        victors.map(victor => {
-          const rainbowName = rainbowize(victor.name);
-          const rainbowDate = rainbowize(victor.date);
-          return (
-            <div className="victor">
-              <span>{rainbowName}</span>
-              <span>{rainbowDate}</span>
-            </div>
-          );
-        })
+        victors.map((victor, i) => (
+          <div className="victor" key={i}>
+            <h3>{rainbowize(victor.name)}</h3>
+            <h3>{rainbowize(victor.date)}</h3>
+          </div>
+        ))
       ) : (
         <p>No victors currently</p>
       )}
       <style jsx>{`
+        .victor-container {
+          font-family: monospace;
+        }
+
         .victor {
           display: flex;
           flex-direction: row;
           justify-content: space-between;
+          font-size: 1.2em;
+          margin: 0px 0px;
+        }
+
+        .victor h3 {
+          margin: 15px 0px;
+        }
+
+        .input-name {
+          background: transparent;
+          display: block;
+          outline: none;
+          border: none;
+          border-bottom: solid 2px #e9eded;
+          margin: auto;
+          margin-bottom: 20px;
+          font-family: monospace;
           font-size: 1.5em;
-          margin: 10px 0px;
+          color: #e9eded;
+        }
+
+        .text-center {
+          text-align: center;
+        }
+
+        p.text-center {
+          color: #e9eded;
+        }
+
+        h3.text-center {
+          font-family: SEGA, monospace;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          text-rendering: optimizeLegibility;
+          white-space: pre-wrap;
+          line-height: 1.2em;
         }
       `}</style>
     </div>
