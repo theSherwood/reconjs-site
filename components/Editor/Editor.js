a1285c6a20e278419fc6143847360932b1d7d332
admls <adam.the.sherwood@gmail.com>
2019-06-13 21:07:32 +0100

    remove breachId from state only on success

--- components/Editor/Editor.js
+++ components/Editor/Editor.js
@@ -59,9 +59,11 @@ const Editor = () => {
       })
     })
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        console.log(data);
+        setBreachId("");
+      })
       .catch(err => console.log(err));
-    setBreachId("");
   };
 
   const submitCode = () => {
