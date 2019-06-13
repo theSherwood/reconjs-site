ddb9ac69634aaca9f0cea2bfbe834dce5a4e65da
admls <adam.the.sherwood@gmail.com>
2019-06-13 14:57:11 +0100

    adds message when there are no errors

--- components/EditorError/EditorError.js
+++ components/EditorError/EditorError.js
@@ -1,6 +1,26 @@
+import React, { Fragment } from "react";
+
 const EditorError = props => {
   const { errors } = props;
-  if (!errors || !Object.keys(errors).length) return null;
+  if (!errors || !Object.keys(errors).length)
+    return (
+      <Fragment>
+        <p>
+          See if you can beat ReconJS by changing the value of window.target
+        </p>
+        <p>
+          Submit your code using the button at the bottom-left or using
+          Ctrl-Enter
+        </p>
+        <style jsx>{`
+          p {
+            color: #f07669;
+            line-height: 1.2em;
+          }
+        `}</style>
+      </Fragment>
+    );
+
   let errs;
   if (Array.isArray(errors)) {
     errs = errors.map(error => {
