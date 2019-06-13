32a6f363283f694f80c9b9873add337ca9f622ba
admls <adam.the.sherwood@gmail.com>
2019-06-13 13:34:33 +0100

    sizes modal

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -17,24 +17,49 @@ const Modal = ({ errors, showVictors, onKeyDown }) => {
   return (
     <Fragment>
       <div className="modal-container" onKeyDown={onKeyDown}>
-        <p>Here is some text</p>
         {errors ? <EditorError errors={errors} /> : null}
-        {showVictors ? <Victors victors={mockVictors} /> : null}
+        {showVictors ? <Victors victors={mockVictors} newVictor /> : null}
       </div>
       <style jsx>{`
         .modal-container {
           position: absolute;
           left: 0;
           right: 0;
-          top: 0;
-          bottom: 0;
-          border-radius: 50% / 10%;
+          top: 9%;
+          bottom: 9%;
           background: #263238;
-          padding: 2.5em 1.5em;
+          padding: 0.5em 0.5em;
+          margin: 0em 0.5em;
           overflow-y: auto;
           font-family: monospace;
           font-size: 1.5em;
           z-index: 4;
+          scrollbar-color: #546e7a transparent;
+          scrollbar-width: thin;
+        }
+
+        .modal-container::-webkit-scrollbar {
+          width: 0.5em;
+        }
+
+        .modal-container::-webkit-scrollbar-track {
+          background: transparent;
+        }
+
+        .modal-container::-webkit-scrollbar-thumb {
+          background-color: #546e7a;
+        }
+
+        @media only screen and (max-width: 600px) {
+          .modal-container {
+            margin: 0em 0.25em;
+          }
+        }
+
+        @media only screen and (max-width: 500px) {
+          .modal-container {
+            margin: 0em 0.1em;
+          }
         }
       `}</style>
     </Fragment>
