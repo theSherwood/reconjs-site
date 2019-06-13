f87263ca31837fdcaaef9a72c37bbff51bc95126
admls <adam.the.sherwood@gmail.com>
2019-06-13 17:57:38 +0100

    move editor/demo to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,28 @@
 import React from "react";
-import Link from "next/link";
+import { StateProvider } from "../state/GlobalState";
+import Editor from "../components/Editor/Editor";
 
 export default () => (
-  <ul>
-    <li>
-      <Link href="/about" as="/about">
-        <a>about</a>
-      </Link>
-    </li>
-    <li>
-      {/* This doesn't work for some reason */}
-      <Link href="/demo" as="/demo">
-        {/* <a href="/demo">demo</a> */}
-        <a>demo</a>
-      </Link>
-    </li>
-    {/* <li>
-      <Link href={{ pathname: '/posts', query: { id: '2' } }} as='/posts/2'>
-        <a>post #2</a>
-      </Link>
-    </li> */}
-  </ul>
+  <StateProvider>
+    <div id="demo-page">
+      <Editor />
+      <style jsx>
+        {`
+          #demo-page {
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0;
+            background: rgb(173, 32, 7);
+            background: radial-gradient(
+              circle,
+              rgba(173, 32, 7, 1) 69%,
+              rgba(27, 27, 27, 1) 100%
+            );
+            padding: 0;
+          }
+        `}
+      </style>
+    </div>
+  </StateProvider>
 );
